99be88d1c75496b094277ce1d53f881b16329d5c
sonsu <tjdtn951007@gmail.com>
2024-05-20 01:18:00 +0900

    쿼리 메서드를 이용한 상품 조회

--- src/main/java/com/shoppingmall/backend/repository/ItemRepository.java
+++ src/main/java/com/shoppingmall/backend/repository/ItemRepository.java
@@ -3,5 +3,10 @@ package com.shoppingmall.backend.repository;
 import com.shoppingmall.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    // NOTE: find + (엔티티 이름) + By + 변수이름
+    List<Item> findByItemNm(String itemNm);
 }
