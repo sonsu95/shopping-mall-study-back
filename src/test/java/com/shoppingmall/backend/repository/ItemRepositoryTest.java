952c045356eafab2efa4a0e7cb1a1d98fb267ee1
sonsu <tjdtn951007@gmail.com>
2024-05-25 01:47:47 +0900

    fix: typo

--- src/test/java/com/shoppingmall/backend/repository/ItemRepositoryTest.java
+++ src/test/java/com/shoppingmall/backend/repository/ItemRepositoryTest.java
@@ -55,20 +55,6 @@ class ItemRepositoryTest {
         System.out.println(savedItem.toString());
     }
 
-    public void createItemList() {
-        for(int i = 1; i <= 10; i++) {
-            Item item = new Item();
-            item.setItemNm("테스트 상품" + i);
-            item.setPrice(10000 + i);
-            item.setItemDetail("테스트 상품 상세 설명" + i);
-            item.setItemSellStatus(ItemSellStatus.SELL);
-            item.setStockNumber(100);;
-            item.setRegTime(LocalDateTime.now());
-            item.setUpdateTime(LocalDateTime.now());
-            Item savedItem = itemRepository.save(item);
-        }
-    }
-
     @Test
     @DisplayName("상품명 조회 테스트")
     public void findByItemNmTest() {
@@ -88,7 +74,7 @@ class ItemRepositoryTest {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
         assertFalse(itemList.isEmpty(), "Item list should not be empty");
-        assertEquals(2, itemList.size(), "There should be exactly two item with the '테스트 상품1' and '테스트 상품5");
+        assertEquals(2, itemList.size(), "There should be exactly two item with the '테스트 상품1' and '테스트 상품5'");
 
 
         for (Item item : itemList) {
