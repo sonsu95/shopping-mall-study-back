aa0cac3b84ff4b89e71908dedf95fcb5c06b3b73
sonsu <tjdtn951007@gmail.com>
2024-05-27 23:14:08 +0900

    @Query를 이용한 검색 처리하기

--- src/main/java/com/shoppingmall/backend/repository/ItemRepository.java
+++ src/main/java/com/shoppingmall/backend/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shoppingmall.backend.repository;
 
 import com.shoppingmall.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +17,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
