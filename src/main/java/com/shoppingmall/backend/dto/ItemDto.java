745761cdb87b0370671a55ed10cac5df864677ad
sonsu <tjdtn951007@gmail.com>
2024-06-20 07:01:27 +0900

    feat: 스프링부트 벨리데이션 추가

--- src/main/java/com/shoppingmall/backend/dto/ItemDto.java
+++ src/main/java/com/shoppingmall/backend/dto/ItemDto.java
@@ -1,5 +1,9 @@
 package com.shoppingmall.backend.dto;
 
+import com.shoppingmall.backend.constant.ItemSellStatus;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +13,24 @@ import java.time.LocalDateTime;
 @Setter
 public class ItemDto {
     private Long id; // 상품 코드
+
+    @NotBlank(message = "상품명은 필수 입력 값입니다.")
     private String itemNm; // 상품명
-    private int price; // 가격
-    private int stockNumber; // 재고수량
+
+    @NotNull(message = "가격은 필수 입력 값입니다.")
+    @PositiveOrZero(message = "가격은 0 이상이어야 합니다.")
+    private Integer price; // 가격
+
+    @NotNull(message = "재고수량은 필수 입력 값입니다.")
+    @PositiveOrZero(message = "재고수량은 0 이상이어야 합니다.")
+    private Integer stockNumber; // 재고수량
+
+    @NotBlank(message = "상품 상세 설명은 필수 입력 값입니다.")
     private String itemDetail; // 상품 상세 설명
-    private String itemSellStatus; // 상품 판매 상태 코드
+
+    @NotNull(message = "상품 판매 상태는 필수 입력 값입니다.")
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태
+
     private LocalDateTime regTime; // 등록 시간
     private LocalDateTime updateTime; // 수정 시간
-
-
 }
